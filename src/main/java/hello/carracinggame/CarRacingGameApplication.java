05a57fc7202145167f1de960c6729bb98d7c2377
chanhl22 <lch9502@naver.com>
2023-02-02 21:17:57 +0900

    프로젝트 생성

---
+++ src/main/java/hello/carracinggame/CarRacingGameApplication.java
@@ -0,0 +1,13 @@
+package hello.carracinggame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRacingGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRacingGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/carracinggame/CarRacingGameApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.carracinggame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRacingGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
