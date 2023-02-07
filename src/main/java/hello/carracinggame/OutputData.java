04ba900ecb717d78b6689d1abf124ebf1d50fddf
chanhl22 <lch9502@naver.com>
2023-02-07 17:29:21 +0900

    refactor: 최종 우승자 로직 수정

--- src/main/java/hello/carracinggame/OutputData.java
+++ src/main/java/hello/carracinggame/OutputData.java
@@ -5,6 +5,7 @@ public class OutputData {
     private static final String INPUT_CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
     private static final String EXECUTION_RESULT_MESSAGE = "실행 결과";
+    private static final String WINNER_MESSAGE = "최종 우승자 : ";
 
     public static void printInputData(String carNameString, int tryCount) {
         print(makeInputCarNameMessage(carNameString));
@@ -34,5 +35,9 @@ public class OutputData {
 
     public static String makeExecutionMessage() {
         return EXECUTION_RESULT_MESSAGE;
+    }
+
+    public static void printWinner(String winner) {
+        print(WINNER_MESSAGE + winner);
     }
 }
--- src/main/java/hello/carracinggame/Winner.java
+++ src/main/java/hello/carracinggame/Winner.java
@@ -6,22 +6,19 @@ import java.util.List;
 public class Winner {
 
     public static void findWinner(List<Car> cars) {
-        OutputData.print("최종 우승자 : " + findWinnerResult(cars));
+        OutputData.printWinner(findWinnerResult(cars));
     }
 
     public static String findWinnerResult(List<Car> cars) {
-        ArrayList<String> temp = new ArrayList<>();
         cars.sort((o1, o2) -> o2.getPosition() - o1.getPosition());
-        temp.add(cars.get(0).getName());
+
+        ArrayList<String> winners = new ArrayList<>();
+        winners.add(cars.get(0).getName());
         for (int i = 1; i < cars.size(); i++) {
             if (cars.get(0).getPosition() == cars.get(i).getPosition()) {
-                temp.add(cars.get(i).getName());
+                winners.add(cars.get(i).getName());
             }
         }
-        String[] temp2 = new String[temp.size()];
-        for (int i = 0; i < temp.size(); i++) {
-            temp2[i] = temp.get(i);
-        }
-        return String.join(",", temp2);
+        return String.join(", ", winners);
     }
 }
